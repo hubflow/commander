6ad55777f3a89f9842e5391351306b1425e0e24b
Sebastian Macias <sebastian@sebastianmacias.com>
2019-02-08 20:08:44 -0800

    ..

--- scripts/bash/install.sh
+++ scripts/bash/install.sh
@@ -1,5 +1,5 @@
 echo ""
 echo ""
-echo "# Source Hubflow Commander" >> ~/.bash_aliases
-echo ". ~/go/src/github.com/hubflow/commander/scripts/bash/all.bash" >> ~/.bash_aliases
+echo "# source rocket dos" >> ~/.bash_aliases
+echo ". ~/go/src/github.com/hubflow/dos/scripts/bash/all.bash" >> ~/.bash_aliases
 . ~/.bash_aliases
