471a8758eb5da8536ed0ac790ae7f639fc8ea2e6
Sebastian Macias <sebastian@sebastianmacias.com>
2019-02-08 20:37:25 -0800

    ..

--- scripts/bash/all.bash
+++ scripts/bash/all.bash
@@ -2,32 +2,36 @@
 # https://misc.flogisoft.com/bash/tip_colors_and_formatting
 # https://en.wikipedia.org/wiki/MS-DOS
 # source commom
-echo 'file:// $dr/github.com/hubflow/dos/scripts/bash/all.bash'
+# echo 'file:// $dr/github.com/hubflow/dos/scripts/bash/all.bash'
 export DEVROOT=$HOME/go/src
 export DR=$HOME/go/src
+export DOSROOT=$HOME/go/src/github.com/hubflow/dos
 
 echo -e '\e[92m
-................................................................
+═══════════════════════════════════════════════════════════════════════════════════
 
 
-    __  __      __    ______                 ____  ____  _____
-   / / / /_  __/ /_  / __/ /___ _      __   / __ \/ __ \/ ___/
-  / /_/ / / / / __ \/ /_/ / __ \ | /| / /  / / / / / / /\__ \
- / __  / /_/ / /_/ / __/ / /_/ / |/ |/ /  / /_/ / /_/ /___/ /
-/_/ /_/\__,_/_.___/_/ /_/\____/|__/|__/  /_____/\____//____/
+  ██████╗  ██████╗  ██████╗██╗  ██╗███████╗████████╗    ██████╗  ██████╗ ███████╗
+  ██╔══██╗██╔═══██╗██╔════╝██║ ██╔╝██╔════╝╚══██╔══╝    ██╔══██╗██╔═══██╗██╔════╝
+  ██████╔╝██║   ██║██║     █████╔╝ █████╗     ██║       ██║  ██║██║   ██║███████╗
+  ██╔══██╗██║   ██║██║     ██╔═██╗ ██╔══╝     ██║       ██║  ██║██║   ██║╚════██║
+  ██║  ██║╚██████╔╝╚██████╗██║  ██╗███████╗   ██║       ██████╔╝╚██████╔╝███████║
+  ╚═╝  ╚═╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝╚══════╝   ╚═╝       ╚═════╝  ╚═════╝ ╚══════╝
+
+
 \e[97m
- Where do you want to go today? \e[93m                    v 0.0.1
+ Where do you want to go today? \e[93m                                         v 0.0.1
 \e[92m
-...............................................................
+═══════════════════════════════════════════════════════════════════════════════════
 '
 
-echo -e '\e[97m'
-echo -e 'to start type: dos ↵\n'
+. $DOSROOT/scripts/bash/common/apps.bash
+. $DOSROOT/scripts/bash/common/docker.bash
+. $DOSROOT/scripts/bash/common/file-management.bash
+. $DOSROOT/scripts/bash/common/go.bash
+. $DOSROOT/scripts/bash/common/misc.bash
+. $DOSROOT/scripts/bash/common/network.bash
+. $DOSROOT/scripts/bash/common/os.bash
 
-. $DR/github.com/hubflow/dos/scripts/bash/common/apps.bash
-. $DR/github.com/hubflow/dos/scripts/bash/common/docker.bash
-. $DR/github.com/hubflow/dos/scripts/bash/common/file-management.bash
-. $DR/github.com/hubflow/dos/scripts/bash/common/go.bash
-. $DR/github.com/hubflow/dos/scripts/bash/common/misc.bash
-. $DR/github.com/hubflow/dos/scripts/bash/common/network.bash
-. $DR/github.com/hubflow/dos/scripts/bash/common/os.bash
+echo -e '\e[97m'
+echo -e ' to start type: dos ↵\n'
--- scripts/bash/common/misc.bash
+++ scripts/bash/common/misc.bash
@@ -1,3 +1,4 @@
 #fun
 alias weather="curl http://wttr.in/tijuana?m"
 alias lg='lazygit'
+alias dos-update='cd $DOSROOT; git pull; s'
