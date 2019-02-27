6a88e343c47ca5334f3fdcb678e55a237a5667e2
Sebastian Macias <sebastian@sebastianmacias.com>
2019-02-27 06:49:03 -0500

    Added new aliases

--- scripts/bash/common/apps.bash
+++ scripts/bash/common/apps.bash
@@ -3,4 +3,6 @@ alias open-navicat="cd ~/Apps/navicat/; ./start_navicat"
 alias open-navicat-fix="del ~/.navicat64; cd ~/Apps/navicat/; ./start_navicat"
 alias open-balsamiq="playonlinux --run 'Balsamiq Mockups 3'"
 
+alias bc='bc -l'
+
 alias k-code="sudo killall code"
--- scripts/bash/common/file-management.bash
+++ scripts/bash/common/file-management.bash
@@ -7,18 +7,61 @@ alias del="trash-put"       # del / trash are shorter than rmtrash
 alias rm="echo Use 'del', or the full path i.e. '/bin/rm'"
 
 
-alias .~='cd ~'
+
 alias .,='cd ..'
 alias ..='cd ../..'
 alias ...='cd ../../..'
-alias ....='cd ../../../..'
-alias .....='cd ../../../..'
+alias ....=' cd ../../../..'
+alias .....='cd ../../../../..'
 alias .4='cd ../../../..'
 alias .5='cd ../../../../..'
 
+
+alias .~='cd ~'
+alias .h='.~'
 alias .d="cd ~/Downloads"
 alias .r="cd $DR"
 alias .rg="cd $DR/github.com"
 alias .rs="cd $DR/source.hubflow.io"
+alias .rk="cd $DR/keybase.io"
+
+# perform both mkdir and cd at the same time using a single command
+function mkdir. () { mkdir -p "$@" && eval cd "\"\$$#\""; }
+
+# clear
+alias c='clear'
+
+## Colorize the ls output ##
+alias ls='ls --color=auto'
+## Use a long listing format ##
+alias ll='ls -la'
+## Show hidden files ##
+alias l.='ls -d .* --color=auto'
 
 alias s=". ~/.bashrc"
+
+# Prints disk usage in human readable form
+alias d="du -sh"
+
+## Simple untar
+alias untar='tar -zxvf '
+
+## Want to download something but be able to resume if something goes wrong?
+alias wget='wget -c '
+
+## Need to generate a random, 20-character password for a new online account? No problem.
+alias getpass="openssl rand -base64 20"
+
+## Downloaded a file and need to test the checksum? We've got that covered too.
+alias sha='shasum -a 256 '
+
+## A normal ping will go on forever. We don't want that. Instead, let's limit that to just five pings.
+alias ping='ping -c 5'
+
+## Want to know how fast your network is? Just download Speedtest-cli and use this alias. You can choose a server closer to your location by using the speedtest-cli --list command.
+alias speed='speedtest-cli --server 2406 --simple'
+## list avaialable servers
+alias speed-list='speedtest-cli --list'
+
+## How many times have you needed to know your external IP address and had no idea how to get that info? Yeah, me too.
+alias ipe='curl ipinfo.io/ip'
