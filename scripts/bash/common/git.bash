78c50b377890993681e50953da00e69d14a07745
Sebastian Macias <sebastian@sebastianmacias.com>
2019-03-08 21:14:21 -0500

    Reenabling GIT

--- scripts/bash/common/git.bash
+++ scripts/bash/common/git.bash
@@ -1,5 +1,3 @@
-# # https://github.com/GitAlias/gitalias
-# git config --local include.path $DOSROOT/scripts/bash/common/.gitalias.txt
-
-
-# git config --local alias.cpr '!f() { echo "Checking out PR: #"$1; echo "Checking out PR: #"$1;  }; f'
+# https://github.com/GitAlias/gitalias
+git config --global include.path $DOSROOT/scripts/bash/common/.gitalias.txt
+# git config --global alias.cpr '!f() { echo "Checking out PR: #"$1; echo "Checking out PR: #"$1;  }; f'
