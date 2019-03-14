9dcd628870cc2adbded8bb7b2a5e5d66b9038027
Sebastian Macias <sebastian@sebastianmacias.com>
2019-03-13 21:34:58 -0500

    Squash info

--- scripts/bash/common/apps.bash
+++ scripts/bash/common/apps.bash
@@ -2,6 +2,7 @@
 alias open-navicat="cd ~/Apps/navicat/; ./start_navicat"
 alias open-navicat-fix="del ~/.navicat64; cd ~/Apps/navicat/; ./start_navicat"
 alias open-balsamiq="playonlinux --run 'Balsamiq Mockups 3'"
+alias coder="cd ~/Apps/code-server/; ./code-server ~/go/src/source.hubflow.io/hubflow/core"
 
 alias bc='bc -l'
 
--- scripts/bash/common/git.bash
+++ scripts/bash/common/git.bash
@@ -1,3 +1,8 @@
 # https://github.com/GitAlias/gitalias
 git config --global include.path $DOSROOT/scripts/bash/common/.gitalias.txt
 # git config --global alias.cpr '!f() { echo "Checking out PR: #"$1; echo "Checking out PR: #"$1;  }; f'
+
+# https://github.com/todotxt/todo.txt-android/wiki/Squash-All-Commits-Related-to-a-Single-Issue-into-a-Single-Commit
+# https://www.devroom.io/2011/07/05/git-squash-your-latests-commits-into-one/
+# https://codewithhugo.com/change-the-date-of-a-git-commit/
+# GIT_COMMITTER_DATE="$(date)" git commit --amend --no-edit --date "$(date)"
