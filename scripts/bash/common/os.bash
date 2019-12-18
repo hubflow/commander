6d47624865d08304e24f9b1e7a8a2805232ce81f
Seb <sebastian@scaletech.co>
2019-12-17 16:24:41 -0800

    Added fix-duplicate-package-repos cmd

--- scripts/bash/common/os.bash
+++ scripts/bash/common/os.bash
@@ -1,5 +1,8 @@
 # os
+
+CLEAN_UP_REPOS_LATEST_RELEASE="https://github.com/davidfoerster/aptsources-cleanup/releases/download/v0.1.6.6/aptsources-cleanup.zip"
 alias reboot='echo "Rebooting system"; sudo reboot'
 alias update='sudo apt-get update'
 alias upgrade='update; sudo apt-get upgrade'
 alias clear-swap="sudo swapoff -a && sudo swapon -a"
+alias fix-duplicate-package-repos='cd /home/$USER/Downloads; wget $CLEAN_UP_REPOS_LATEST_RELEASE; sudo python3 -OEs aptsources-cleanup.zip'
--- scripts/bash/install.sh
+++ scripts/bash/install.sh
@@ -1,5 +1,9 @@
 echo ""
 echo ""
+echo "Intalling dependencies"
+echo "sudo apt install trash-cli"
+echo "sudo apt install python3-apt"
+echo "Intalling dependencies         ... done"
 echo "# source rocket dos" >> ~/.bash_aliases
 echo ". ~/go/src/github.com/hubflow/dos/scripts/bash/all.bash" >> ~/.bash_aliases
 . ~/.bash_aliases
