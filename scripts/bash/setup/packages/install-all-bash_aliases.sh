eb92a9f2b450dce22d8afbf073c67213606c8c8d
Seb <sebastian@scaletech.co>
2019-12-19 02:04:11 -0800

    ...

--- scripts/bash/setup/packages/install-all-bash_aliases.sh
+++ scripts/bash/setup/packages/install-all-bash_aliases.sh
@@ -1,14 +1,15 @@
 echo "Install bash_aliases    ...begins" 
 
 touch ~/.bash_projects
-echo "# Projects bash_aliases" >> ~/.bash_projects
+echo "" >> ~/.bash_projects
 sed -i '1s;^;# Projects bash_aliases\n;' ~/.bash_projects
 
 
 touch ~/.bash_aliases
-echo "# rocket dos begins" >> ~/.bash_aliases
-sed -i '1s;^;# rocket dos ends\n;' ~/.bash_aliases
+echo "" >> ~/.bash_aliases
+# sed -i '1s;^;# rocket dos ends\n;' ~/.bash_aliases
+sed -i '1s;^;. ~/.bash_projects\n;' ~/.bash_aliases
 sed -i '1s;^;. ~/go/src/github.com/hubflow/dos/scripts/bash/all-bash_aliases.bash\n;' ~/.bash_aliases
-sed -i '1s;^;# rocket dos begins\n;' ~/.bash_aliases
+# sed -i '1s;^;# rocket dos begins\n;' ~/.bash_aliases
 
 echo "install bash_aliases    ...done" 
