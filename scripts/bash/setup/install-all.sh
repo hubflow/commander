84810a7fc215cd461c32ca5cacff9e325c0e0a0a
Seb <sebastian@scaletech.co>
2019-12-19 00:10:06 -0800

    Re-organizing code

--- scripts/bash/setup/install-all.sh
+++ scripts/bash/setup/install-all.sh
@@ -1,9 +1,9 @@
 echo ""
 echo ""
 echo "Intalling dependencies"
-. ~/go/src/github.com/hubflow/dos/scripts/bash/setup/install-base-dependencies.sh
-. ~/go/src/github.com/hubflow/dos/scripts/bash/setup/install-firacode.sh
-. ~/go/src/github.com/hubflow/dos/scripts/bash/setup/install-code.sh
+. ~/go/src/github.com/hubflow/dos/scripts/bash/setup/packages/install-base-dependencies.sh
+. ~/go/src/github.com/hubflow/dos/scripts/bash/setup/packages/install-firacode.sh
+. ~/go/src/github.com/hubflow/dos/scripts/bash/setup/packages/install-code.sh
 echo "# source rocket dos" >> ~/.bash_aliases
 echo ". ~/go/src/github.com/hubflow/dos/scripts/bash/all.bash" >> ~/.bash_aliases
 . ~/.bash_aliases
