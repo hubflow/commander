6c4901a33d7cf50bf2238471f918c4361d509518
Seb <sebastian@scaletech.co>
2019-12-19 02:16:44 -0800

    ..

--- scripts/bash/setup/packages/install-all-bash_aliases.sh
+++ scripts/bash/setup/packages/install-all-bash_aliases.sh
@@ -4,12 +4,22 @@ touch ~/.bash_projects
 echo "" >> ~/.bash_projects
 sed -i '1s;^;# Projects bash_aliases\n;' ~/.bash_projects
 
+# remove  duplicates
+sort ~/.bash_projects | uniq > .bash_projects.new
+mv ~/.bash_projects  ~/.bash_projects.backup
+mv ~/.bash_projects.new  ~/.bash_projects
+
 
 touch ~/.bash_aliases
 echo "" >> ~/.bash_aliases
-# sed -i '1s;^;# rocket dos ends\n;' ~/.bash_aliases
 sed -i '1s;^;. ~/.bash_projects\n;' ~/.bash_aliases
 sed -i '1s;^;. ~/go/src/github.com/hubflow/dos/scripts/bash/all-bash_aliases.bash\n;' ~/.bash_aliases
-# sed -i '1s;^;# rocket dos begins\n;' ~/.bash_aliases
+
+
+# remove  duplicates
+sort ~/.bash_aliases | uniq > .bash_aliases.new
+mv ~/.bash_aliases  ~/.bash_aliases.backup
+mv ~/.bash_aliases.new  ~/.bash_aliases
+
 
 echo "install bash_aliases    ...done" 
