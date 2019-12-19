175fdf283a4a9dc4f7ef70be43b5a62999ff9e3d
Seb <sebastian@scaletech.co>
2019-12-19 00:18:31 -0800

    ..

--- scripts/bash/setup/install-all.sh
+++ scripts/bash/setup/install-all.sh
@@ -7,3 +7,5 @@
 # echo "# source rocket dos" >> ~/.bash_aliases
 # echo ". ~/go/src/github.com/hubflow/dos/scripts/bash/all.bash" >> ~/.bash_aliases
 # . ~/.bash_aliases
+
+echo 'INSTALLING DOS   ...done'
